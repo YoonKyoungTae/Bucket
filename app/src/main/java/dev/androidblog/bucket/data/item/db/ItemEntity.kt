c1c245830bce29abef52418d1ec577e5b0596986
diordna <yoon91.dev@gmail.com>
2022-09-14 01:50:56 +0900

    Edit Entity (add title, filterType)

--- app/src/main/java/dev/androidblog/bucket/data/item/db/ItemEntity.kt
+++ app/src/main/java/dev/androidblog/bucket/data/item/db/ItemEntity.kt
@@ -3,10 +3,19 @@ package dev.androidblog.bucket.data.item.db
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * title
+ * data
+ * filterType 필터링될 옵션 (url, file, number....)
+ * createAt
+ * updateAt
+ */
 @Entity
 data class ItemEntity(
     @PrimaryKey(autoGenerate = true) val id: Int,
+    val title: String,
     val data: String,
+    val filterType: String,
     val createAt: Long,
     val updateAt: Long,
 )
